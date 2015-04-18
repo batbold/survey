6eaa236ea513159b33e223b338904f90001197f5
Batbold <batbold.n@grapecity.mn>
2015-04-18 17:11:30 +0900

    tutorial root lesson

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  # get 'auth/index'
+  # root 'auth#index'
+  match ':controller(/:action(/:id))', :via => :get
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
