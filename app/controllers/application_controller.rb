05b5bb1b9bd677ca47d3b1396d9bccbaa5d0efa9
Batbold <batbold.n@grapecity.mn>
2015-04-23 14:27:48 +0900

    Бааз шилжүүлэв

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   layout "login"
   protect_from_forgery with: :exception
   helper_method :current_user
+  helper_method :data
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,22 @@
 class User < ActiveRecord::Base
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth["provider"]
+      user.uid = auth["uid"]
+      user.name = auth["info"]["name"]
+      user.image = auth["info"]["image"]
+    end
+  end
+
   def self.from_omniauth(auth)
-	  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-	    user.provider = auth.provider 
-	    user.uid      = auth.uid
-	    user.name     = auth.info.name
-	    user.save
-	  end
-end
+	puts auth.inspect
+  	puts "simba haana bbnaa"
+	where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+	user.provider = auth.provider 
+	user.uid      = auth.uid
+	user.name     = auth.info.name
+    user.image 	  = auth.info.image
+	user.save
+  end
+	end
 end
